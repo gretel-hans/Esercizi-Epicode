9a56be8aef63693264e0408d56747a6db0c8069a
Adjei Hansel Sarpong <adjeihansel@icloud.com>
2023-02-06 16:10:32 +0100

    primo es js

---
+++ Week-2/G1/assets/js/primo.js
@@ -0,0 +1,3 @@
+window.alert("Usa il pop up per dare dei messaggi");
+window.alert("Sto studiando JS");
+document.write("<h1>Iniziamo la nostra pratica</h1> <p>output scritto da Javascript</p>");
---
+++ Week-2/G1/assets/js/secondo.js
@@ -0,0 +1,11 @@
+var testo="pantera scritto da Javascript"
+document.write(testo);
+
+var primoAnimale="criceto";
+console.log(primoAnimale);
+
+var SecondoAnimale="giraffa";
+console.log(SecondoAnimale);
+
+var terzo_Animale="leone";
+console.log(terzo_Animale);
